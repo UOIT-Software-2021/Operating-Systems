5682d36533e37de3d4de47c8ed4c50845a403f7d
Daniel <djnucci@hotmail.com>
2020-01-20 14:43:53 -0500

    finished task1

--- task1.c
+++ task1.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main(void){
-    
+    int data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+    int i;
+    for (i=0; i < 10; i++){
+        printf("%d ",data[i]);
+    }
+    printf("\n");
 }
