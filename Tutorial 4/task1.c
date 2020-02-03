404c0be617c84a3e642715b2a486c125be81600b
Daniel <djnucci@hotmail.com>
2020-02-03 16:40:05 -0500

    Task1 finit

--- Tutorial 4/task1.c
+++ Tutorial 4/task1.c
@@ -1,6 +1,21 @@
+#define _XOPEN_SOURCE 600 
+#include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
-int main(int argc, const char* argv[])
-{
-	
+void *hello_world() {
+	printf("Hello world! \n");
+}
+
+void *goodbye() {
+	printf("Goodbye! \n");
+}
+
+pthread_t thread1, thread2;
+
+int main() {
+	printf("%d \n", pthread_create(&thread1, NULL, hello_world, (void*)0)); 
+    printf("%d \n", pthread_create(&thread2, NULL, goodbye, 1)); 
+
+	return 0;
 }
