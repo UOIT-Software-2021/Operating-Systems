1a575e47f3d23b17affec1eace86e11e8418827e
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-02-27 16:17:03 -0500

    Rough version of shell

---
+++ Lab 2/myshell.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h> 
+#include "myshell.h"
+
+#define test 1
+int main (int argc, char *argv[])
+{
+	//create variables that need independence from loop
+	char pwd[256]; 
+	getcwd(pwd,256);
+	char *command;
+	FILE *batchptr;
+	if (argc > 1){
+		batchptr = fopen(argv[1], "r");
+	}
+	while (1) {
+		//stores command from user
+		char input[512];
+		char allWhiteSpace = 1;
+		while (allWhiteSpace){
+			//get command from user or from batch file
+			input[0]=' ';
+			if (argc == 1){
+				
+				printf("%s/myshell $ ", pwd);
+				
+				//read entire line
+				scanf("%[^\n]%*c", input);
+				//printf("%d %lu\n",input[0], strlen(input));
+				//return 0;			
+			} else {
+				if (fscanf(batchptr, "%[^\n]%*c", input) == EOF) {
+					return 0;//quit shell if at end of batch file
+				}
+			}
+			for (int i =0; i < strlen(input); i++){
+				if (!isspace(input[i])&&!(input[0]==1)) {
+					allWhiteSpace = 0;
+					break;
+				}
+			}
+		}
+		//copy of input to use as backup
+		char inputCopy[512];
+		strcpy(inputCopy, input);
+
+		//first pass of command: count number of tokens
+		int argCount = 0;
+		char *token = strtok(input, " ");
+
+		while(token != NULL){
+			argCount++;
+
+			token = strtok(NULL, " ");			
+						
+	    	}
+
+		//restore input
+		strcpy(input, inputCopy);
+		//reset token for second pass
+		token = strtok(input, " ");
+							
+		//to store arguments		
+		char *arguments[argCount];
+
+		//first token is the desired command
+		command = malloc(sizeof(token));
+		strcpy(command, token);
+
+		//get next token
+		token = strtok(NULL, " ");
+		
+		for (int i = 0; i < argCount; i++){
+			
+			//get next token
+			if (token != NULL){
+				if (strcmp(token, "<")==0){
+				//handle input redirection
+				}
+				else if (strcmp(token, ">")==0){
+					//handle output redirection with truncation
+				}
+				else if (strcmp(token, ">>")==0){
+					//handle output redirection with appending
+				}
+				else if (strcmp(token, "&")==0){
+					//handle process in the background
+				}
+				else {
+					//get next argument
+					//arguments[i] =char[256];
+					arguments[i] = malloc(sizeof(token));
+					strcpy(arguments[i], token);
+
+								
+		    		}
+
+				token = strtok(NULL, " ");			
+			}
+ 
+			//all tokens have been read: set remaining args to null
+			else {
+				//last arg should be null when using exec
+				arguments[i] = NULL;
+			}			
+		}
+		
+		//restore input
+		strcpy(input, inputCopy);
+		printf("ln 113 %s.%lu\n", command, strlen(command));
+		if (strcmp(command, "quit")==0){
+			//exit loop
+			free(command);	
+			for (int i = 0; i < argCount; i++){
+				free(arguments[i]);
+			}			
+			break; 
+		} 
+
+		else if (strcmp(command, "cd")==0){
+			token = strtok(NULL, " ");
+			if (token == NULL){
+				printf("%s\n", pwd);
+			}
+			else {
+				//actually change directory
+			}
+		} 
+
+		else if (strcmp(command, "clr")==0){
+			//clear screen
+		} 
+
+		else if (strcmp(command, "dir")==0){
+			//print directory contents
+			readDirectoryContent(pwd);
+		} 
+
+		else if (strcmp(command, "environ")==0){
+			//list environment strings???
+		} 
+
+		else if (strcmp(command, "echo")==0){
+
+			for (int i = 5; i < strlen(input); i++){				
+				printf("%c",input[i]);
+			}			
+			printf("\n");
+		} 
+		
+		else if (strcmp(command, "help")==0){
+			//display help manual
+		}
+		
+		else if (strcmp(command, "pause")==0){
+			//pause (should be easy enough)
+		} 
+		else if(strlen(command)>0){
+			//attempt to execute the command
+			if (executeCommand(command, arguments) == -1){
+				printf("myshell: %s: Could not execute\n", command);
+			}
+		}
+		free(command);
+		for (int i = 0; i < argCount; i++){
+			free(arguments[i]);
+		}
+	}
+	
+
+	return 0;
+}
+
