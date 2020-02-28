9ba33c2e95414ce1a55fa6929d4bee172a80aede
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-02-28 02:21:28 -0500

    Added Environ, Added Help
    Additionally tweaked:
    -cd changes are reflected in working directory environment variable
    -fixed dir: prints items in current directory with no arguments and prints the items in a supplied directory
    -fixed echo: now is compliant with IO redirection if that ever gets fixed
    -not sure if the help command is what is required but it works

--- Lab 2/myshell.c
+++ Lab 2/myshell.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <ctype.h>
+#include <ctype.h> 
 #include "myshell.h"
+
+extern char **environ;
 
 void deleteEndStr (char* str, char del){
 
@@ -20,28 +22,56 @@ void deleteEndStr (char* str, char del){
     return;
 }
 
-#define test 1
 int main (int argc, char *argv[])
 {
 	//create variables that need independence from loop
+
+	//working directory
 	char pwd[256]; 
 	getcwd(pwd,256);
+	
+	//path to manual
+	char manualPath[256];
+	strcpy(manualPath,pwd);
+	strcat(manualPath, "/readme");
+	putenv(manualPath);
+
+	//path to shell
+	char shell[256];
+	strcpy(shell,pwd);
+	strcat(shell, "/myshell");
+	setenv("shell", shell, 1);	
+	
+	//string to hold shell command
 	char *command;
+
+	//pointer to possible batch file
 	FILE *batchptr;
 	if (argc > 1){
 		batchptr = fopen(argv[1], "r");
+	} else {
+		//clear screen if human user
+		clearScreen();
 	}
+	
 	while (1) {
 		//stores command from user
 		char input[512];
 		char allWhiteSpace = 1;
+
+		//hold copies of stdin or out if needed		
+		int stdin_copy, stdout_copy;	
+		
+		//set certain execution statuses
+		int restoreoriginalstdin = 0, restoreoriginalstdout = 0, notbgprocess = 1;
+		//do not progress if buffer only has white space		
 		while (allWhiteSpace){
 			//get command from user or from batch file
 			//input[0]=' ';
 			if (argc == 1){
-				input[0]='\n';
+				printf("\033[0;36m"); 
 				printf("%s/myshell $ ", pwd);
-				
+				printf("\033[0m"); 
 				//read entire line
 				fgets(input, 512, stdin);
 			} else {
@@ -50,6 +80,7 @@ int main (int argc, char *argv[])
 					return 0;//quit shell if at end of batch file
 				}
 			}
+			//check each character in input buffer and break if not space
 			for (int i =0; i < strlen(input); i++){
 				if (!isspace(input[i])) {
 					allWhiteSpace = 0;
@@ -57,6 +88,7 @@ int main (int argc, char *argv[])
 				}
 			}
 		}
+		//set last character from '\n' to 0
 		input[strlen(input)-1]=0;
 		//copy of input to use as backup
 		char inputCopy[512];
@@ -81,35 +113,71 @@ int main (int argc, char *argv[])
 		//to store arguments		
 		char *arguments[argCount];
 
-		//first token is the desired command
-		command = malloc(sizeof(token));
-		strcpy(command, token);
+		//needed in the case of io redirection		
+		char *infname;	
+		char *outfname;
 
+		//first token is the desired command
+		command = strdup(token);
 		//get next token
 		token = strtok(NULL, " ");
-		
+		int tempargcount = 0;
 		for (int i = 0; i < argCount; i++){
 			
 			//get next token
 			if (token != NULL){
 				if (strcmp(token, "<")==0){
-				//handle input redirection
+					token = strtok(NULL, " ");
+					infname = strdup(token);
+					if( access( infname, F_OK|R_OK ) != -1 && !restoreoriginalstdin) {
+						//dup2(stdin_copy, 0);
+						
+					      	freopen(infname, "r", stdin);
+						  
+				      		restoreoriginalstdin = 1;
+					} else {
+						command = strdup("");
+						perror("myshell: cannot open file to read");	 
+					}
 				}
 				else if (strcmp(token, ">")==0){
-					//handle output redirection with truncation
+					token = strtok(NULL, " ");
+					outfname = strdup(token);
+					printf("%s\n",outfname);
+					if((access(outfname, W_OK ) != -1 || access(outfname, F_OK) == -1)&& !restoreoriginalstdout) {
+						//dup2(stdout_copy, 1);
+						
+					      	freopen(outfname, "w", stdout);
+						//freopen("test.txt", "w", stdout);  
+					
+						restoreoriginalstdout = 1;
+					} else {
+						strcpy(command, "");
+						perror("myshell: cannot open file to write");	 
+					}
 				}
-				else if (strcmp(token, ">>")==0){
-					//handle output redirection with appending
+				else if (strcmp(token, ">>")==0){	
+					token = strtok(NULL, " ");
+					outfname = strdup(token);
+					if(( access(outfname, W_OK ) != -1 || access(outfname, F_OK) == -1)&& !restoreoriginalstdout) {
+						//dup2(stdout_copy, 1);
+					      	freopen(outfname, "a", stdout);  
+				      		restoreoriginalstdout = 1;
+					} else {
+						strcpy(command, "");
+						perror("myshell: cannot open file append"); 
+					
+					}
 				}
 				else if (strcmp(token, "&")==0){
 					//handle process in the background
+					notbgprocess = 0;
+					
 				}
 				else {
 					//get next argument
-					//arguments[i] =char[256];
-					arguments[i] = malloc(sizeof(token));
-					strcpy(arguments[i], token);
-
+					arguments[i] = strdup (token);
+					tempargcount++;
 								
 		    		}
 
@@ -119,76 +187,112 @@ int main (int argc, char *argv[])
 			//all tokens have been read: set remaining args to null
 			else {
 				//last arg should be null when using exec
-				arguments[i] = NULL;
+				arguments[i]=NULL;
+				tempargcount++;
+				break;
 			}			
 		}
-		
-		//restore input
-		strcpy(input, inputCopy);
+		argCount = tempargcount;
+
+		//long if-else chain to choose appropriate command
 		if (strcmp(command, "quit")==0){
-			//exit loop
+			//exit loop: then exit shell
 			free(command);	
-			for (int i = 0; i < argCount; i++){
+			for (int i = 0; i < argCount; i++){			
 				free(arguments[i]);
 			}			
 			break; 
 		} 
 
 		else if (strcmp(command, "cd")==0){
-			token = strtok(arguments[0], " ");
-			if (token == NULL){
+			
+			//select option based on number of args
+			if (argCount==1){
 				printf("%s\n", pwd);
 			}
-			else if (strcmp(arguments[0],"..")==0){
-				deleteEndStr(pwd,'/');
-			}
-			else if(access(arguments[0], F_OK) != -1){
-				strcat(pwd, "/");
-				strcat(pwd, token); 
+			else if(checkIfDirectory(arguments[0])){
+				chdir(arguments[0]);
+				getcwd(pwd,256);
 			} else {
 				printf("Directory does not exist!\n");
 			}
 		} 
 
 		else if (strcmp(command, "clr")==0){
+			//clear screen
 			clearScreen();
 		} 
 
 		else if (strcmp(command, "dir")==0){
-			readDirectoryContent(pwd);
+			//print directory contents
+			if (argCount == 1){
+				readDirectoryContent(pwd);
+			} else {
+				if( readDirectoryContent(arguments[0])) {
+					printf("myshell: error: could not open specified directory\n");
+				}
+			}
+		
 		} 
 
 		else if (strcmp(command, "environ")==0){
-			//list environment strings???
+			//list environment strings
+			printEnv(environ);
 		} 
 
 		else if (strcmp(command, "echo")==0){
-
-			for (int i = 5; i < strlen(input); i++){				
-				printf("%c",input[i]);
-			}			
-			printf("\n");
+			//echo rest of line
+			echo(argCount, arguments);
 		} 
 		
 		else if (strcmp(command, "help")==0){
 			//display help manual
+			readMan(manualPath);
 		}
 		
 		else if (strcmp(command, "pause")==0){
+			//pause the shell
 			pauseShell();
 		} 
 		else if(strlen(command)>0){
 			//attempt to execute the command
-			if (executeCommand(command, arguments) == -1){
+			if (executeCommand(command, arguments, notbgprocess)== -1){
 				printf("myshell: %s: Could not execute\n", command);
 			}
 		}
+
+		//free variables
 		free(command);
 		for (int i = 0; i < argCount; i++){
+			
 			free(arguments[i]);
 		}
+
+
+		//restore stdin and stdout
+		if(restoreoriginalstdin){
+			fflush(stdin);  
+              		fclose(stdin);
+			free(infname);              		
+			stdin = fdopen(stdin_copy, "r");
+			restoreoriginalstdin = 0;
+		}
+		if(restoreoriginalstdout){
+			fflush(stdout);  
+              		fclose(stdout);
+
+			free(outfname);
+            		
+			stdout = fdopen(stdout_copy, "w");
+			dup2(stdout_copy, 1);
+
+			close(stdout_copy);
+			restoreoriginalstdout = 0;
+		}
+
 	}
 	
 
 	return 0;
 }
+
