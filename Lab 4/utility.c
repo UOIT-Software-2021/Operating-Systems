1b98a1f807383261e15c5fc3042dae0f971986bd
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 16:24:02 -0400

    Create utility.c

---
+++ Lab 4/utility.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h> 
+#include "queue.h"
+
+
+//i guess this is for exec and other system calls or something
