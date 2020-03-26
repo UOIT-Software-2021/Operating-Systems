6531d3c69cbed4538687fe5e30524aa15a65176d
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 16:54:51 -0400

    Update hostd.c

--- Lab 4/hostd.c
+++ Lab 4/hostd.c
@@ -10,8 +10,47 @@
 #include <string.h> 
 #include "queue.h"
 
+#define MEMORY 1024
+
 int main(void) {
-  
-  
-  return 0; 
+
+	node_t *realtime = NULL;
+	node_t *priority1 = NULL;
+	node_t *priority2 = NULL;
+	node_t *priority3 = NULL;
+	
+	FILE *fp;
+	fp = fopen("dispacthlist", "r");
+	if (fp == NULL){
+		printf("file error!\n");
+		return 1;
+	}
+	//read file line by line untile EOF is reached
+	while (1) {
+		proc process;
+		if (fscanf(fp, "%d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d\n", process.arrivalTime, process.priority, process.processorTime, process.memory, process.printers, process.scanners, process.modems, process.cds, process.pid) == EOF) {
+			break;
+		}
+		//set pid to 0
+		process.pid = 0;
+		//set address to 0
+		process.address = 0;
+		//add to appropriate queue
+		switch (process.priority) {
+			case 0:
+				push (&realtime, process);
+				break;
+			case 1:
+				push (&priority1, process);
+				break;
+			case 2:
+				push (&priority2, process);
+				break;
+			default:
+				push (&priority3, process);
+		}
+	}
+	fclose(fp);
+
+	return 0; 
 }
