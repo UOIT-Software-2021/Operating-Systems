d37e764660d5cd6cd7035b212971569e078390ac
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 16:16:02 -0400

    Update hostd.c

--- Lab 4/hostd.c
+++ Lab 4/hostd.c
@@ -1,1 +1,11 @@
-
+#define _POSIX_SOURCE //required to not get warning for using kill
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h> 
+#include "queue.h"
