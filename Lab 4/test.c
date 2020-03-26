8fe751420f0ddaf11d5084e9a0e0b6cc7eeab02b
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 16:23:20 -0400

    not sure what test.c is supposed to do, but is in the makefile

---
+++ Lab 4/test.c
@@ -0,0 +1,17 @@
+#define _POSIX_SOURCE //required to not get warning for using kill
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h> 
+#include "queue.h"
+
+int main(void) {
+  
+  
+  return 0; 
+}
