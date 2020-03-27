bcc328835e4ec61f9118cbf2ffcf8245c19656b2
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-27 01:44:47 -0400

    Update hostd.h

--- Lab 4/hostd.h
+++ Lab 4/hostd.h
@@ -1,5 +1,6 @@
 #define MEMORY 1024
 
+//resources structure
 typedef struct {
     int printers, scanners, modems, cds;
 } resources;
@@ -7,3 +8,9 @@ typedef struct {
 int set_mem(int *avail_mem, int memory, int p);
 
 void free_mem(int *avail_mem, int address, int memory, int val);
+
+void pushByQueue(node_t **rt, node_t **p1, node_t **p2, node_t **p3, proc process, int level);
+
+int realtimeIterate(node_t **realtime, int * avail_mem, resources res_avail);
+
+int priorityIterate(node_t **priority1, node_t **priority2, node_t **priority3, int * avail_mem, resources res_avail, int level);
