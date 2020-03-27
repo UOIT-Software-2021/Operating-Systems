1d86415c5b438e65bcd38da0ceb9c997319b1c1d
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 23:30:15 -0400

    Update hostd.h

--- Lab 4/hostd.h
+++ Lab 4/hostd.h
@@ -3,3 +3,7 @@
 typedef struct {
     int printers, scanners, modems, cds;
 } resources;
+
+int set_mem(int *avail_mem, int memory, int p);
+
+void free_mem(int *avail_mem, int address, int memory, int val);
