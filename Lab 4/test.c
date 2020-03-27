6093bb0bb672271187461289541725662ee8e10b
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 23:28:26 -0400

    Update test.c

--- Lab 4/test.c
+++ Lab 4/test.c
@@ -1,4 +1,3 @@
-#define _POSIX_SOURCE //required to not get warning for using kill
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -11,7 +10,21 @@
 #include "queue.h"
 
 int main(void) {
-  
-  
-  return 0; 
+	node_t *head = NULL;
+	proc t;
+	t.arrivalTime = 1;
+	t.priority = 2;
+	t.processorTime = 3;
+	t.memory = 4;
+	t.printers = 3;
+	t.scanners = 5;
+	t.modems = 8;
+	t.cds = 10;
+	t.pid=0;
+	
+	push(&head, t);
+	
+
+	proc g = pop(&head);
+	printf("%d, %d, %d, %d, %d, %d, %d, %d, %d\n", g.arrivalTime, g.priority, g.processorTime, g.memory, g.printers, g.scanners, g.modems, g.cds, g.pid);
 }
