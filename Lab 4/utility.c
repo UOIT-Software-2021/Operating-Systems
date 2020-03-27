0d882484bff3859c74ad8d9285e2c4a772d8fb3c
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-26 23:29:33 -0400

    Update utility.c

--- Lab 4/utility.c
+++ Lab 4/utility.c
@@ -1,3 +1,4 @@
+#define _POSIX_SOURCE //required to not get warning for using kill
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,6 +9,41 @@
 #include <sys/wait.h>
 #include <string.h> 
 #include "queue.h"
+#include "hostd.h"
 
+int set_mem(int *avail_mem, int memory, int p){
+	int address = 0;
+	int maxmem = MEMORY - 65;	
+	//start at dedicated time memory location	
+	if (p == 0){
+		address = maxmem;
+		maxmem = MEMORY;
+	}
+	//iterate through each memory address
+	while (address < maxmem && memory <= maxmem){
+		int allocate = 1;
+		//check if there is available address with enough consecutive memory        
+		for (int i = address; i < address+memory && i < maxmem; i++){
+		    //break if allocated memory is found            
+		    if(avail_mem[i]){
+			allocate = 0;
+			break;
+		    }
+		}
+		//return if allocation is ok
+		if(allocate){
+		    //set memory to allocated
+		    free_mem(avail_mem, address, memory, 1);
+		    return address;
+		}
+		address ++;
+	}
+	//return negative address to indicate not enough available memory
+	return -1;
+}
 
-//i guess this is for exec and other system calls or something
+void free_mem(int *avail_mem, int address, int memory, int val){
+    for (int i = address; i < address+memory && i < MEMORY; i++){
+        avail_mem[i] = val;
+    }
+}
