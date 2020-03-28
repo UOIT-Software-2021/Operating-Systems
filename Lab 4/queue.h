6887e60e3db35476d37b55962416a20a8412ae8f
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-27 21:42:16 -0400

    Update queue.h

--- Lab 4/queue.h
+++ Lab 4/queue.h
@@ -1,16 +1,20 @@
 
 //process structure
 typedef struct {
-    int arrivalTime, priority, processorTime, memory, printers, scanners, modems, cds, pid, suspended, address;
+    int arrivalTime, priority, processorTime, memory, printers, scanners, modems, cds, pid, suspended, address, started;
 } proc;
 
 //queue structure
 typedef struct queue
 {
-    proc data;
+    proc *data;
     struct queue *next;
 } node_t;
 
-void push(node_t **head, proc process);
+void push(node_t **head, proc *process);
 
-proc pop(node_t **head);
+proc *pop(node_t **head);
+
+proc *peek(node_t **head);
+
+int queueSize(node_t **head);
