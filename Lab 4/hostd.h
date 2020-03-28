2494f0340c03b8111c3ca8cc5a4e49a2b3405f44
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-27 21:41:14 -0400

    Update hostd.h

--- Lab 4/hostd.h
+++ Lab 4/hostd.h
@@ -9,8 +9,9 @@ int set_mem(int *avail_mem, int memory, int p);
 
 void free_mem(int *avail_mem, int address, int memory, int val);
 
-void pushByQueue(node_t **rt, node_t **p1, node_t **p2, node_t **p3, proc process, int level);
+void pushByQueue(node_t **rt, node_t **p1, node_t **p2, node_t **p3, proc *process);
 
 int realtimeIterate(node_t **realtime, int * avail_mem, resources res_avail);
 
-int priorityIterate(node_t **priority1, node_t **priority2, node_t **priority3, int * avail_mem, resources res_avail, int level);
+
+int priorityIterate(node_t **realtime, node_t **priority1, node_t **priority2, node_t **priority3, int * avail_mem, resources res_avail, int currTime, int test);
