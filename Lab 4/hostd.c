c43bd8718c9779c2a72e6e677a3d0a26d4cb97b3
OwenStoll99 <48807960+OwenStoll99@users.noreply.github.com>
2020-03-27 21:40:56 -0400

    Update hostd.c

--- Lab 4/hostd.c
+++ Lab 4/hostd.c
@@ -15,11 +15,13 @@
 
 
 
-resources res_avail;
+
 
 int main(void) {
 	int avail_mem[MEMORY];
+	resources res_avail;
 
+	
 	//use to initialize avail_mem to 0
     	free_mem(avail_mem, 0, MEMORY, 0);
 	
@@ -33,7 +35,7 @@ int main(void) {
 	res_avail.modems = 1;
 	res_avail.cds = 2;
 	
-
+	int timePassed = -1;
 	FILE *fp;
 	fp = fopen("dispatchlist", "r");
 	if (fp == NULL){
@@ -42,30 +44,31 @@ int main(void) {
 	}
 	//read file line by line untile EOF is reached
 	while (1) {
-		proc process;
+		proc *process = (proc *) malloc(sizeof(proc));
 		//read all relevant data from the line in the file
-		if (fscanf(fp, "%d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d\n", &process.arrivalTime, &process.priority, &process.processorTime, &process.memory, &process.printers, &process.scanners, &process.modems, &process.cds) == EOF) {
+		if (fscanf(fp, "%d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d%*c %d\n", &process->arrivalTime, &process->priority, &process->processorTime, &process->memory, &process->printers, &process->scanners, &process->modems, &process->cds) == EOF) {
 			break;
 		}
-
-		//set pid to 0
-		process.pid = 0;
-		//set address to 0
-		process.address = 0;
-		//set suspended to 0
-		process.suspended = 0;
-		//add to appropriate queue
-		pushByQueue(&realtime, &priority1, &priority2, &priority3, process, process.priority);
-	}
-	fclose(fp);
+		//set timePassed to start at initial arrival time
+		if (timePassed < 0){
+			timePassed = process->arrivalTime;
+		}
 		
-	realtimeIterate(&realtime, avail_mem, res_avail);
+		//set pid to 0
+		process->pid = 0;
+		//set address to 0
+		process->address = 0;
+		//set suspended to 0
+		process->suspended = 0;
+		//add to appropriate queue
+		process->started = -1;
 
-	priorityIterate(&priority1, &priority2, &priority3, avail_mem, res_avail, 1);
+		
+		pushByQueue(&realtime, &priority1, &priority2, &priority3, process);
+	}
+	fclose(fp);		
 	
-	priorityIterate(NULL, &priority2, &priority3, avail_mem, res_avail, 2);
-	
- 	priorityIterate(NULL, NULL, &priority3, avail_mem, res_avail, 3);
+	priorityIterate(&realtime, &priority1, &priority2, &priority3, avail_mem, res_avail, timePassed, 0);
 	
 	return 0; 
 }
